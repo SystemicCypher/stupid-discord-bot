09575e3ef47f88cf835ca2d8bdeabd820e322e8b
Austin <austinloza24@gmail.com>
2020-12-02 16:31:36 -0800

    Cleaned up and added switch/case
    The if statements were annoying. And I thought it looked better if I trimmed the ! from all the commands.

--- index.js
+++ index.js
@@ -10,35 +10,42 @@ client.on('ready', () => {
   });
   
 client.on('message', msg => {
-    if (msg.content === '!about') {
-        msg.reply("Hi! I'm a lil' bot. I don't do much, but I promise I'll get smarter in time!");
-    }
+    if (msg.content.startsWith('!')){
+        var args = msg.content.substring(1).split(" ")
+        const cmd = args[0]
+        const channel = msg.channel
 
-    if (msg.content === '!sheev') {
-        msg.channel.send(sheev.PALPS_IMG)
-        msg.reply(sheev.PALPS)
-    }
-
-    if (msg.content.startsWith('!treason')) {
-        msg.channel.send(sheev.TREASON_IMG)
-        msg.reply(sheev.TREASON)
-    }
-
-    if (msg.content === '!power'){
-        msg.channel.send(sheev.POWER_IMG)
-        msg.reply(sheev.POWER)
-    }
-
-    if (msg.content === '!game'){
-        const embed = new Discord.MessageEmbed()
-                            .setTitle('A Game Embed')
-                            .setColor(0xaaaa00)
-                            .setDescription('This is a game')
-        msg.channel.send(embed)
+        switch(cmd){
+            case "help":
+                const embed = new Discord.MessageEmbed()
+                                    .setTitle("Available Commands")
+                                    .setDescription("All commands so far:\n!about\n!sheev\n!treason\n!power")
+                channel.send(embed)
+                break
+            case "about":
+                msg.reply("Hi! I'm a lil' bot. I don't do much, but I promise I'll get smarter in time!")
+                break
+            case "sheev":
+                channel.send(sheev.PALPS_IMG)
+                msg.reply(sheev.PALPS)
+                break
+            case "treason":
+                channel.send(sheev.TREASON_IMG)
+                msg.reply(sheev.TREASON)
+                break
+            case "power":
+                channel.send(sheev.POWER_IMG)
+                msg.reply(sheev.POWER)
+                break
+            case "game":
+                const gameembed = new Discord.MessageEmbed()
+                    .setTitle('A Game Embed')
+                    .setColor(0xaaaa00)
+                    .setDescription('This is a game')
+                channel.send(gameembed)
+                break
+        }
     }
 });
-
-
-
   
-  client.login(ENV.TOKEN);
+client.login(ENV.TOKEN);
