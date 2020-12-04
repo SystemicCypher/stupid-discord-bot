4eb5c639487c24756e1cab06388c91e756bd78f8
Austin <austinloza24@gmail.com>
2020-12-03 17:27:32 -0800

    created function for pkmn image

--- pokemon.js
+++ pokemon.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js')
 
-const pkmn_gif_dp = "https://www.serebii.net/pokedex-dp/icon/"
-const pkmn_gif_filetype = ".gif"
-
 const game_name = "Discordmon"
+
+function getPKMNImage(pokemon_number){
+    var stringified_num = ""
+    if(pokemon_number < 10){
+        stringified_num = "00" + pokemon_number;
+    }
+    else if(pokemon_number < 100){
+        stringified_num = "0" + pokemon_number;
+    }
+    else{
+        stringified_num = "" + pokemon_number;
+    }
+    return ("https://www.serebii.net/pokedex-dp/icon/"+ stringified_num + ".gif")
+}
 
 const pokemon_game = (args, channel) => {
     const pokemon_embed = new Discord.MessageEmbed()
                                 .setTitle(game_name)
-                                .setImage(pkmn_gif_dp + args[1] + pkmn_gif_filetype)
+                                .setImage(getPKMNImage(args[1]))
+    console.log(args[1])
     channel.send(pokemon_embed)
 
 }
